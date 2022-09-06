4a11e8a983e371682e8909304c1be5ce2735935a
Tyler Hall <thall2980@gmail.com>
2022-09-06 13:46:23 -0400

    commit tree a02bb74ce71b6dd7827c9067ddc54eacdfb98236

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+function submitData() {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((formData) => document.querySelector("body").append(formData.id))
+    .catch((error) => document.querySelector("body").append(error));
+}
